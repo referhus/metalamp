a1fa95802d7368bd359602f9af99db9d2c0f71ba
Winterh <kotfimn@mail.ru>
2021-09-29 20:33:44 +0700

    components

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -2510,7 +2510,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61711" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62716" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
